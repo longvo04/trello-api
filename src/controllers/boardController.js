f7dc7dee729790e90925a6ff94aee602d87f15ce
Natori <longvo1903@gmail.com>
2025-07-19 15:41:54 +0700

    service layer base

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,8 +1,12 @@
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
+
 
 const createNew = async (req, res, next) => {
   try {
-    res.status(StatusCodes.CREATED).json({ message: 'Board created successfully' })
+    const newBoard = await boardService.createNew(req.body)
+
+    res.status(StatusCodes.CREATED).json(newBoard)
   } catch (error) {
     next(error)
   }
