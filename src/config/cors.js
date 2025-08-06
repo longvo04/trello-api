6d208f52df15d997c4a98c8af0ea0ca5634aafbb
Natori <longvo1903@gmail.com>
2025-08-06 21:55:46 +0700

    Producttion Deploy (Render)

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,14 +4,15 @@ import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
 
 export const corsOptions = {
-  origin: function (origin, callback) {if (!origin && env.BUILD_MODE === 'dev') {
-    return callback(null, true)
-  }
-  if (WHITELIST_DOMAINS.includes(origin)) {
-    return callback(null, true)
-  }
+  origin: function (origin, callback) {
+    if (env.BUILD_MODE === 'dev') {
+      return callback(null, true)
+    }
+    if (WHITELIST_DOMAINS.includes(origin)) {
+      return callback(null, true)
+    }
 
-  return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
+    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
   },
 
   optionsSuccessStatus: 200,
--- src/server.js
+++ src/server.js
@@ -17,10 +17,17 @@ const START_SERVER = () => {
   app.use(express.json())
   app.use('/v1', APIs_V1)
   app.use(errorHandlingMiddleware)
-  app.listen(port, hostname, () => {
+  if (env.BUILD_MODE === 'production') {
+    app.listen(process.env.port, () => {
     // eslint-disable-next-line no-console
-    console.log(`Server is running at http://${ hostname }:${ port }/`)
-  })
+      console.log(`Server is running at port ${ process.env.port }`)
+    })
+  } else {
+    app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+      console.log(`Server is running at http://${ hostname }:${ port }/`)
+    })
+  }
 
   exitHook(() => CLOSE_DB())
 }
